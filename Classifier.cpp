0a77c6527a97eb026c6b77dcefd8569f7f1e9d03
Rich Dammkoehler <rpd@noradltd.com>
2012-08-27 08:44:59 -0700

    Rich D: moved all dangling methods into the class as private methods

--- Classifier.cpp
+++ Classifier.cpp
@@ -5,32 +5,32 @@
 using namespace Triangles;
 using namespace std;
 
-bool isZero(int d)
+bool Classifier::isZero(int d)
 {
   return 0 == d;
 }
 
-bool hasLengths(Triangle triangle)
+bool Classifier::hasLengths(Triangle triangle)
 {
   return !(isZero(triangle.getLength0()) &&
 	   isZero(triangle.getLength1()) &&
 	   isZero(triangle.getLength2()));
 }
 
-bool hasThreeEqualSides(Triangle triangle) 
+bool Classifier::hasThreeEqualSides(Triangle triangle) 
 {
   return triangle.getLength0() == triangle.getLength1() &&
     triangle.getLength0() == triangle.getLength2();
 }
 
-bool hasTwoEqualSides(Triangle triangle)
+bool Classifier::hasTwoEqualSides(Triangle triangle)
 {
   return triangle.getLength0() == triangle.getLength1() ||
     triangle.getLength0() == triangle.getLength2() ||
     triangle.getLength1() == triangle.getLength2();
 }
 
-Classifier::Type classifyByLengths(Triangle triangle)
+Classifier::Type Classifier::classifyByLengths(Triangle triangle)
 {
   Classifier::Type type = Classifier::invalid;
   if (hasThreeEqualSides(triangle))
@@ -48,36 +48,36 @@ Classifier::Type classifyByLengths(Triangle triangle)
   return type;
 }
 
-bool isNinty(float angle)
+bool Classifier::isNinty(float angle)
 {
   return fabs(90.0f - angle) < 0.0001;
 }
 
-bool greaterThanNinty(float angle)
+bool Classifier::greaterThanNinty(float angle)
 {
   return angle > 90.0f;
 }
 
-bool lessThanNinty(float angle) 
+bool Classifier::lessThanNinty(float angle) 
 {
   return angle < 90.0f;
 }
 
-bool hasNintyDegreeAngle(Triangle triangle)
+bool Classifier::hasNintyDegreeAngle(Triangle triangle)
 {
   return isNinty(triangle.getAngle0()) ||
     isNinty(triangle.getAngle1()) ||
     isNinty(triangle.getAngle2());
 }
 
-bool hasObtuseAngle(Triangle triangle)
+bool Classifier::hasObtuseAngle(Triangle triangle)
 {
   return greaterThanNinty(triangle.getAngle0()) ||
     greaterThanNinty(triangle.getAngle1()) ||
     greaterThanNinty(triangle.getAngle2());
 }
 
-Classifier::Type classifyByAngles(Triangle triangle)
+Classifier::Type Classifier::classifyByAngles(Triangle triangle)
 {
   Classifier::Type type = Classifier::invalid;
   if (hasNintyDegreeAngle(triangle))
@@ -95,53 +95,53 @@ Classifier::Type classifyByAngles(Triangle triangle)
   return type;
 }
 
-float sumAngles(Triangle triangle)
+float Classifier::sumAngles(Triangle triangle)
 {
   return triangle.getAngle0() + triangle.getAngle1() + triangle.getAngle2();
 }
 
-bool equal(float a, float b) 
+bool Classifier::equal(float a, float b) 
 {
   return fabs( a - b ) < 0.0001;
 }
 
-bool isZero(float f) 
+bool Classifier::isZero(float f) 
 {
   return equal(0.0f, f);
 }
 
-bool isOneEighty(float f) 
+bool Classifier::isOneEighty(float f) 
 {
   return equal(180.0f, f);
 }
 
-bool isNegative(float f)
+bool Classifier::isNegative(float f)
 {
   return 0.0f > f;
 }
 
-bool hasNegativeAngles(Triangle triangle)
+bool Classifier::hasNegativeAngles(Triangle triangle)
 {
   return isNegative(triangle.getAngle0()) || isNegative(triangle.getAngle1()) || isNegative(triangle.getAngle2());
 }
 
-bool validAngles(Triangle triangle)
+bool Classifier::validAngles(Triangle triangle)
 {
   float sumOfAngles = sumAngles(triangle);
   return !hasNegativeAngles(triangle) && ( isZero(sumOfAngles) || isOneEighty(sumOfAngles) );
 }
 
-bool isNegative(int d)
+bool Classifier::isNegative(int d)
 {
   return 0 > d;
 }
 
-bool lengthIsValid(int length) 
+bool Classifier::lengthIsValid(int length) 
 {
   return !isZero(length) && !isNegative(length);
 }
 
-bool lengthsAreValid(Triangle triangle)
+bool Classifier::lengthsAreValid(Triangle triangle)
 {
   bool valid = true;
   valid &= lengthIsValid(triangle.getLength0());
@@ -150,7 +150,7 @@ bool lengthsAreValid(Triangle triangle)
   return valid;
 }
 
-bool longestSideIsntTooLong(Triangle triangle)
+bool Classifier::longestSideIsntTooLong(Triangle triangle)
 {
   int longest = 0;
   int shortSideSum = 0;
@@ -172,7 +172,7 @@ bool longestSideIsntTooLong(Triangle triangle)
   return longest < shortSideSum;
 }
 
-bool validLengths(Triangle triangle) 
+bool Classifier::validLengths(Triangle triangle) 
 {
   bool valid = true;
   if ( hasLengths(triangle) )
@@ -183,7 +183,7 @@ bool validLengths(Triangle triangle)
   return valid;
 }
 
-bool valid(Triangle triangle)
+bool Classifier::valid(Triangle triangle)
 {
   return validAngles(triangle) && validLengths(triangle);
 }
--- Classifier.h
+++ Classifier.h
@@ -28,6 +28,32 @@ namespace Triangles
       };
 
     Type classify(Triangle);
+
+  private:
+    bool isZero(int);
+    bool hasLengths(Triangle);
+    bool hasThreeEqualSides(Triangle);
+    bool hasTwoEqualSides(Triangle);
+    Type classifyByLengths(Triangle);
+    bool isNinty(float);
+    bool greaterThanNinty(float);
+    bool lessThanNinty(float);
+    bool hasNintyDegreeAngle(Triangle);
+    bool hasObtuseAngle(Triangle);
+    Type classifyByAngles(Triangle);
+    float sumAngles(Triangle);
+    bool equal(float, float);
+    bool isZero(float);
+    bool isOneEighty(float);
+    bool isNegative(float);
+    bool hasNegativeAngles(Triangle);
+    bool validAngles(Triangle);
+    bool isNegative(int);
+    bool lengthIsValid(int);
+    bool lengthsAreValid(Triangle);
+    bool longestSideIsntTooLong(Triangle);
+    bool validLengths(Triangle);
+    bool valid(Triangle);
   };
 
 }
